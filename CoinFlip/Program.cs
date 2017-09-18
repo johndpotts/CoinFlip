73139e25576d4aece621072e7ab4ab3562a1ee0c
John Potts <john@arlingtonchurch.com>
2017-09-18 05:57:39 -0400

    refactor

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -10,6 +10,36 @@ namespace CoinFlip
     {
         static void Main(string[] args)
         {
+            int coin;
+            string userGuess;
+            string coinDescription;
+            Random rng = new Random();
+
+            // Query the user
+            Console.Write("Enter your guess- Heads or Tails (H or T)");
+            userGuess = Console.ReadLine();
+
+            //get a random number for coin flip
+            coin = rng.Next(0, 2);
+            if (coin == 1)
+            { coinDescription = "Heads"; }
+            else
+            {
+                coinDescription = "Tails";
+            }
+
+            if ((coin == 0 && userGuess == "T" )|| (coin ==1 && userGuess == "H"))
+            {
+                Console.WriteLine("The coin flip was {0}, you win!!",coinDescription);
+            }
+          
+            else
+            {
+               
+                    Console.WriteLine("The coin flip was {0}, you lose", coinDescription);
+                
+            }
+            Console.ReadLine();
         }
     }
 }
